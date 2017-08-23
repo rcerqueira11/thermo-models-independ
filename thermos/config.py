85c5cea1fabb3ef1c91d5bc7de1a6d93255ad2fe
Ricardo Cerqueira <raxs64@gmail.com>
2017-08-23 18:32:50 -0400

    primero commit

---
+++ thermos/config.py
@@ -0,0 +1,37 @@
+import os
+
+basedir= os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = '\x05\xa5\xa6\xb8\xe8\x06\x0f\xa9\x98\xf4\xf8\xe5H\x92j9\xab\x16\xe5\xe0\xa8\x9e\xb9\x92'
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    # DEBUG = True
+    # url = 'postgresql://{}:{}@{}:{}/{}'
+    # url = url.format(user, password, host, port, db)
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/wordcount_dev'
+
+class SQLiteDevelopmentConfig(Config):
+    # DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'thermos.db')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'thermos.db')
+
+
+config_by_name = dict(
+    dev = DevelopmentConfig,
+    sqlitedev = SQLiteDevelopmentConfig,
+    test = TestingConfig,
+    prod = ProductionConfig
+)
+
